25ba42a3d06bbb4475043a7eac311c010aa72b09
darkapplepower <chictaehee@naver.com>
2020-12-29 21:02:25 +0900

    Create 가르치기.js

---
+++ functions/가르치기.js
@@ -0,0 +1,34 @@
+const garuchigi=function(){
+    const DATA=JSON.parse(DataBase.getDataBase("darkbot/학습/data")===null?DataBase.setDataBase("darkbot/학습/data","{}"):DataBase.getDataBase("darkbot/학습/data"));
+    const z="@@";
+    return {
+        g:function(msg,room){
+            if(!DATA.hasOwnProperty(room))DATA[room]={};
+            msg=msg.split("@");
+            if(msg.length>=2){
+                if(msg[1]!==""){
+                    if(!DATA[room].hasOwnProperty(msg[0])){
+                        DATA[room][msg[0]]=msg[1];
+                        DataBase.setDataBase("darkbot/학습/data",JSON.stringify(DATA));
+                        return "성공적으로 학습했습니다.";
+                    }else{
+                        return "이미 배운 단어입니다.";
+                    }
+                }else{
+                    return "응답할 말은 한 글자 이상이어야 합니다.";
+                }
+            }else{
+                return "형식이 잘못되었습니다.\n올바른 형식: /가르치기 가르칠 말@이렇게 답하기";
+            }
+        },
+        u:function(msg,room){
+            if(!DATA.hasOwnProperty(room))DATA[room]={};
+            msg=msg.replace(z,"");
+            if(DATA[room].hasOwnProperty(msg)){
+                return DATA[room][msg];
+            }else{
+                return false;
+            }
+        }
+    };
+}();
