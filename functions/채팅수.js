066286a6b6d814dfa1346b1cb8964c2e6a09f392
darkapplepower <chictaehee@naver.com>
2021-01-11 15:41:30 +0900

    Update 채팅수.js
    bug fixed

--- functions/채팅수.js
+++ functions/채팅수.js
@@ -12,11 +12,11 @@ const chatCount=function(){
             return sort.map((x,xx)=>(xx===3?"\u200b".repeat(1000):"")+(xx+1)+"위 "+x[0]+"님 ("+x[1]+"회)").join("\n");
         },
         plus:function(room,sender){
-            if(!DATA.hasOwnProperty(room)){
+            if(!Object.keys(DATA).includes(room)){
                 let a=DataBase.getDataBase("darkbot/채팅수/"+room);
                 DATA[room]=JSON.parse(a===null?DataBase.setDataBase("darkbot/채팅수/"+room,"{}"):a);
             }
-            if(!DATA[room].hasOwnProperty(sender)){
+            if(!Object.keys(DATA[room]).includes(sender)){
                 DATA[room][sender]=1;
                 return;
             }
